bb20eb0c4baa0f834e786399f3c6a2fe9a6e02d0
ArtemYakushkin <zimberger29101985@gmail.com>
2023-12-04 23:06:47 +0200

    add slick-slider, start adaptiv

---
+++ js/mobile.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('.mobile-menu');
+const btnOpenMenu = document.querySelector('.mobile__btn-open');
+const btnCloseMenu = document.querySelector('.mobile__btn-close');
+
+const toggleMenu = () => menu.classList.toggle('is-open');
+
+btnOpenMenu.addEventListener('click', toggleMenu);
+btnCloseMenu.addEventListener('click', toggleMenu);
